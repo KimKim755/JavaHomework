57bba613cd7ddaf1e5e5b355314ff6d9159391fb
kimkim755 <kphillip002@my.wilmu.edu>
2022-03-15 17:27:35 -0700

    Main.java

---
+++ src/main/java/Dayone.java
@@ -0,0 +1,8 @@
+//variables
+public class Dayone {
+    public static void main(String[] args) {
+        String name = "Kim";
+        System.out.println(name);
+    }
+}
+
---
+++ src/main/java/Main.java
@@ -0,0 +1,137 @@
+ public class Main {
+    public static void main(String[] args) {
+        int myNum = 5;
+        System.out.println(myNum);
+        {
+        }
+    }public class Main {
+            public static void main(String[] args) {
+                String name = "Kim";
+                System.out.println(name);
+            }//type casting
+        }public class Main {
+            public static void main(String[] args) {
+                String txt = "How much wood could a woodchuck chuck If a woodchuck could chuck wood?";
+                System.out.println("The length of the txt string is: " + txt.length());
+            }
+        }public class Main {
+         public static void main(String[] args) {
+             String txt = "Good Morning";
+             System.out.println(txt.toUpperCase());
+             System.out.println(txt.toLowerCase());
+         }//math
+     }public class Main {
+            public static void main(String[] args) {
+                System.out.println(Math.sqrt(80));
+            }
+        public class Main {
+            public static void main(String[] args) {
+                System.out.println(Math.max(10, 20));
+
+            }public class Main {
+                public static void main(String[] args) {
+                    if (30 > 15) {
+                        System.out.println("30 is greater than 15 ");
+                    }
+                }public class Main {
+                    public static void main(String[] args) {
+                        int x = 100;
+                        int y = 99;
+                        if (x > y) {
+                            System.out.println("x is greater than y");
+                        }
+                    }
+                }
+                public class Main {
+                    public static void main(String[] args) {
+                        int time = 40;
+                        if (time < 10) {
+                            System.out.println("Good day.");
+                        } else {
+                            System.out.println("Good evening.");
+
+                        }public class Main {
+                            public static void main(String[] args) {
+                                int day = 6;
+                                switch (day) {
+                                    case 1:
+                                        System.out.println("Monday");
+                                        break;
+                                    case 2:
+                                        System.out.println("Tuesday");
+                                        break;
+                                    case 3:
+                                        System.out.println("Wednesday");
+                                        break;
+                                    case 4:
+                                        System.out.println("Thursday");
+                                        break;
+                                    case 5:
+                                        System.out.println("Friday");
+                                        break;
+                                    case 6:
+                                        System.out.println("Saturday");
+                                        break;
+                                    case 7:
+                                        System.out.println("Sunday");
+                                        break;
+                                }
+                            }
+                        }public class Main {
+                            public static void main(String[] args) {
+                                int i = 0;
+                                while (i < 10) {
+                                    System.out.println(i);
+                                    i++;
+
+
+                                }public class Main {
+                                    public static void main(String[] args) {
+                                        int i = 1;
+                                        do {
+                                            System.out.println(i);
+                                            i++;
+                                        }
+                                        while (i < 11);
+                                    }
+                                }public class Main {
+                                    public static void main(String[] args) {
+                                        for (int i = 0; i < 100; i++) {
+                                            System.out.println(i);
+                                        }
+                                    }public class Main {
+                                        public static void main(String[] args) {
+                                            for (int i = 0; i <= 20; i = i + 5) {
+                                                System.out.println(i);
+                                            }
+                                        }public class Main {
+                                            public static void main(String[] args) {
+                                                String[] cars = {"bmw", "honda", "mercedes", "kia"};
+                                                System.out.println(cars[3]);
+                                            }
+                                        }public class Main {
+                                            static void myMethod() {
+                                                System.out.println("Today was my first day at the internship!");
+                                            }
+
+                                            public static void main(String[] args) {
+                                                myMethod();
+                                            }
+                                        }
+
+
+                                    }
+
+                                }
+
+                            }
+                        }
+
+                    }
+                }
+            }
+        }
+
+
+
+    }}
